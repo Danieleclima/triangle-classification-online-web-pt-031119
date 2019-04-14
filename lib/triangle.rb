de0dbc93681d06469bd1dcb6a5682badc7367f09
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-14 15:54:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Triangle
   
   attr_reader :side1, :side2, :side3, :triangle
@@ -24,6 +26,7 @@ class Triangle
       raise TriangleError
     elsif @side1 == @side2 && @side2 == @side3
       return :equilateral
+    binding.pry
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3 
       return :isosceles 
     else 
